0c57cabafdcb0ba39be53d1abb0eb70035126558
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-15 14:25:07 -0300

    Create And.js
    O operador AND é utilizado para verificar se duas ou mais condições são verdadeiras simultaneamente. Em linguagens de programação, ele retorna True apenas quando todas as expressões envolvidas são verdadeiras. Caso contrário, retorna False. Exemplo: A AND B.

---
+++ And.js
@@ -0,0 +1,9 @@
+// AND ( && ) - Só é verdade, se as duas condicoes forem vdd. 
+// - programa que ve se 100 moedas coletadas e 1 estrela
+
+let moedas = 100
+let item = "estrela"
+
+let resultado = (moedas coletadas = 100) && (item === "Estrela")
+console.log(resultado)
+// vai sair falso
