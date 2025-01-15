667717da9fbd603f3229fef92ed41fc1423c74eb
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-15 14:37:58 -0300

    Create NOT.js
    O operador NOT é utilizado para inverter o valor lógico de uma expressão. Ele retorna True quando a expressão é falsa, e False quando a expressão é verdadeira. É essencial para a criação de condições de negação em expressões lógicas. Exemplo: NOT A.

---
+++ NOT.js
@@ -0,0 +1,16 @@
+// NOT (!) Nega uma afirmação
+
+let tempo = "chuva"
+let banho =  tempo === "chuva"
+console.log (!!banho) 
+// 
+// inverte o resultado // se por dois inverte a inversão 
+
+/*
+  O operador NOT é utilizado para inverter o valor lógico de uma expressão. 
+  Se a expressão for True, ele retorna False; se for False, retorna True.
+  É útil para negar condições, simplificando expressões e ajudando em testes de negação. Exemplo: NOT A.
+Esse operador é bastante útil em situações em que você precisa verificar a negação de uma condição ou inverter o resultado de uma expressão lógica. 
+Se precisar de mais exemplos, estou à disposição!
+
+*/
